37a5585d9311dfec44a8dc679b9b1ecb71cfc669
Vikas <victorjonathon@gmail.com>
2022-07-06 13:06:30 +0530

    Symfony installed and initial template with twig

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     *  @Route("lucky/number", name="luckynumber", methods={"GET","HEAD"})
+     */
+    public function number():Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+
+}
